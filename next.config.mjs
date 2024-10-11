7c1bd028b4641c26517f2be3e3b792ede2e02869
Lars Bättig <lars.baettig@securix.swiss>
2024-10-11 13:45:22 +0200

    add prefix for prod build

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+  basePath: isProd ? '/elastic-docker-images-vuln-report' : '',
+  assetPrefix: isProd ? '/elastic-docker-images-vuln-report/' : '',
   output: 'export',
   trailingSlash: true,
   images: {
